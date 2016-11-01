9ecf3ec7b3ae4a9dc15768bb5fa2fae302aed1fc
Nathaniel Bender <nsbender1@gmail.com>
2016-11-01 19:40:29 -0400

    Persistent Profile Photo/ Cleaner Code

--- src/app/src/main/java/edu/calvin/the_b_team/calvinassassingame/ProfileViewActivity.java
+++ src/app/src/main/java/edu/calvin/the_b_team/calvinassassingame/ProfileViewActivity.java
@@ -2,8 +2,12 @@ package edu.calvin.the_b_team.calvinassassingame;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.preference.PreferenceManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -21,13 +25,18 @@ import android.net.Uri;
 import android.content.SharedPreferences;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 
 public class ProfileViewActivity extends AppCompatActivity {
 
     private ListView mDrawerList;
     private ArrayAdapter<String> mAdapter;
     private DrawerLayout mDrawerLayout;
-    private String mActivityTitle;
     private ActionBarDrawerToggle mDrawerToggle;
 
     private Button choosePhotoButton;
@@ -45,60 +54,35 @@ public class ProfileViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_view);
+        setTitle(getResources().getText(R.string.profile_activity_title));
 
         //Set up the menu drawer and its items
         mDrawerList = (ListView)findViewById(R.id.navList);mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
-        setTitle(getResources().getText(R.string.profile_activity_title));
-
         addDrawerItems();
         setupDrawer();
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
 
+        //Define layout items
         profileImage = (ImageView) findViewById(R.id.profile_picture);
         choosePhotoButton = (Button) findViewById(R.id.choose_photo_button);
         finalizeButton = (Button) findViewById(R.id.finalize_profile_button);
         playerNameEditable = (EditText) findViewById(R.id.player_name_editable);
         playerClassEditable = (EditText) findViewById(R.id.player_class_editable);
         playerHomeEditable = (EditText) findViewById(R.id.player_home_editable);
-
         app_preferences = PreferenceManager.getDefaultSharedPreferences(this);
 
+        //Load layout items from saved preferences
         playerNameEditable.setText(app_preferences.getString("playerName",playerNameEditable.getText().toString()));
         playerClassEditable.setText(app_preferences.getString("playerClass",playerClassEditable.getText().toString()));
         playerHomeEditable.setText(app_preferences.getString("playerHome",playerHomeEditable.getText().toString()));
+        loadProfileImage(app_preferences.getString("playerPhotoPath","android.resource://edu.calvin.the_b_team.calvinassassingame/" + R.mipmap.ic_profile_placeholder));
         settingsFinalized = app_preferences.getBoolean("settingsFinalized",false);
 
+        //Disable editing the fields if they've previously been finalized
         if (settingsFinalized == true){
             disableTextFields();
         }
-
-        final AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
-        dlgAlert.setMessage(R.string.finalize_message);
-        dlgAlert.setTitle("Finalize?");
-        dlgAlert.setCancelable(true);
-        dlgAlert.setPositiveButton("Finalize",
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        settingsFinalized = true;
-                        //Disable the info fields
-                        disableTextFields();
-
-                        SharedPreferences.Editor editor = app_preferences.edit();
-                        editor.putString("playerName", playerNameEditable.getText().toString());
-                        editor.putString("playerClass", playerClassEditable.getText().toString());
-                        editor.putString("playerHome", playerHomeEditable.getText().toString());
-                        editor.putBoolean("settingsFinalized", settingsFinalized);
-                        editor.commit(); // Very important
-                    }
-                });
-        dlgAlert.setNegativeButton("Not yet",
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        //Dismiss confirmation message and return
-                    }
-                });
 
         //Open the photo gallery browser to allow the user to choose a profile photo
         choosePhotoButton.setOnClickListener(new View.OnClickListener() {
@@ -108,6 +92,27 @@ public class ProfileViewActivity extends AppCompatActivity {
                 startActivityForResult(pickPhoto , 1);//one can be replaced with any action code
             }
         });
+
+        //Define the confirmation dialogue to confirm finalizing the profile info
+        final AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
+        dlgAlert.setMessage(R.string.finalize_message);
+        dlgAlert.setTitle("Finalize?");
+        dlgAlert.setCancelable(true);
+        dlgAlert.setPositiveButton("Finalize", new DialogInterface.OnClickListener() {
+            //When the confirm button is pressed, finalize the data
+            public void onClick(DialogInterface dialog, int which) {
+                settingsFinalized = true;
+                //Disable the info fields
+                disableTextFields();
+                finalizeTextFields();
+            }
+        });
+        dlgAlert.setNegativeButton("Not yet", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                //Dismiss confirmation message and return
+            }
+        });
+        //Open the finalize confirmation dialogue when the FINALIZE button is pressed
         finalizeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -117,6 +122,7 @@ public class ProfileViewActivity extends AppCompatActivity {
     }
 
     private void disableTextFields(){
+        //Prevent the user from editing the info on the page
         playerNameEditable.setEnabled(false);
         playerClassEditable.setEnabled(false);
         playerHomeEditable.setEnabled(false);
@@ -124,7 +130,63 @@ public class ProfileViewActivity extends AppCompatActivity {
         finalizeButton.setEnabled(false);
     }
 
-    //Set the profile picture after returning from the photo gallery
+    //Save all of the profile data to a preferences file to be loaded back in when the app is re-opened
+    private void finalizeTextFields(){
+        SharedPreferences.Editor editor = app_preferences.edit();
+        editor.putString("playerName", playerNameEditable.getText().toString());
+        editor.putString("playerClass", playerClassEditable.getText().toString());
+        editor.putString("playerHome", playerHomeEditable.getText().toString());
+        editor.putBoolean("settingsFinalized", settingsFinalized);
+
+        Bitmap profileBitmap = ((BitmapDrawable)profileImage.getDrawable()).getBitmap();
+        editor.putString("playerPhotoPath", saveProfilePhoto(profileBitmap));
+
+        editor.commit(); // Commit the changes to the preferences file
+    }
+
+    //Save the profile image to a file in the app's internal file system (imageDir/profile.jpg)
+    private String saveProfilePhoto(Bitmap bitmapImage){
+        ContextWrapper cw = new ContextWrapper(getApplicationContext());
+        // path to /data/data/yourapp/app_data/imageDir
+        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+        // Create imageDir
+        File mypath=new File(directory,"profile.jpg");
+
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(mypath);
+            // Use the compress method on the BitMap object to write image to the OutputStream
+            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                fos.close();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return directory.getAbsolutePath();
+    }
+
+    //Load the profile image from the hardcoded path that it was saved to.
+    //Loads directly into the profileImage ImageView object
+    private void loadProfileImage(String path) {
+        try {
+            File f=new File(path, "profile.jpg");
+            Bitmap b = BitmapFactory.decodeStream(new FileInputStream(f));
+            profileImage.setImageBitmap(b);
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    //Set the profile picture after user selects it from the photo gallery
     protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
         super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
         switch(requestCode) {
@@ -175,19 +237,9 @@ public class ProfileViewActivity extends AppCompatActivity {
                 invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
             }
         };
-
         mDrawerToggle.setDrawerIndicatorEnabled(true);
         mDrawerLayout.setDrawerListener(mDrawerToggle);
     }
-
-
-//    This is an expiriment. It adds the '...' to the action bar
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -200,12 +252,10 @@ public class ProfileViewActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         // Activate the navigation drawer toggle
         if (mDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -236,9 +286,6 @@ public class ProfileViewActivity extends AppCompatActivity {
                 break;
         }
     }
-
     //End of menu drawer configuration
-
-
 
 }
